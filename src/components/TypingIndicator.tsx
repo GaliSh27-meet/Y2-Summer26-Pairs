e0ec5033e86984fcda86fb4ca5900c94201c5a3e
GaliSh27-meet <glgl.shech@gmail.com>
2026-07-20 13:26:33 +0300

    iooiohio

--- src/components/TypingIndicator.tsx
+++
@@ -1,9 +0,0 @@
-export default function TypingIndicator() {
-  return (
-    <div className="typing">
-      <span className="dot" />
-      <span className="dot" />
-      <span className="dot" />
-    </div>
-  );
-}
